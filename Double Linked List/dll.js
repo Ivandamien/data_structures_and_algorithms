80ec04f0921bd4c9fb9ff7dbd2596afb9b1d5ed3
Ivan Okello <ivandamieno99@gmail.com>
2022-10-18 22:48:39 +0300

    started on double linked list

---
+++ Double Linked List/dll.js
@@ -0,0 +1,23 @@
+// Node
+// -val
+//-prev
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+
+    }
+}
+
+// DoublyLinkedList
+// -Head
+//-length
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
+    }
+}
