50ec57da3d0180df6393f8ba4bde23a1e9392855
Ivan Okello <ivandamieno99@gmail.com>
2022-10-19 09:22:00 +0300

    Implemented push method

--- Double Linked List/dll.js
+++ Double Linked List/dll.js
@@ -20,4 +20,18 @@ class DoublyLinkedList {
         this.length = 0;
 
     }
+    push(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+
+        }
+        this.length++;
+        return this;
+    }
 }
