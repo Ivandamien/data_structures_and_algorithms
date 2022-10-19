4b289dcf2f45e8787e1eff8be3e4f1e81d9e2e05
Ivan Okello <ivandamieno99@gmail.com>
2022-10-19 12:34:26 +0300

    Added insert method

--- Double Linked List/dll.js
+++ Double Linked List/dll.js
@@ -105,4 +105,18 @@ class DoublyLinkedList {
         }
         return false;
     }
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return this.unshift(val);
+        if (index === this.length) return this.push(val);
+        var newNode = new Node(val);
+        var beforeNode = this.get(index - 1);
+        var afterNode = beforeNode.next;
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode.
+        afterNode.prev = newNode
+        this.length++;
+        return true;
+    }
 }
