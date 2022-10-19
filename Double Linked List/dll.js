026c857907fcf3aa47906efb23eb6093d9c34b4d
Ivan Okello <ivandamieno99@gmail.com>
2022-10-19 11:06:14 +0300

    Implemented unshift method solution

--- Double Linked List/dll.js
+++ Double Linked List/dll.js
@@ -62,4 +62,16 @@ class DoublyLinkedList {
         this.length--;
         return oldHead;
     }
+    unshift(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
 }
