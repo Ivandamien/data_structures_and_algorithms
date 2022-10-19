2fa3719fe5699889db3639b239986aa8e581bc74
Ivan Okello <ivandamieno99@gmail.com>
2022-10-19 09:40:46 +0300

    Created a pop method in DLL class

--- Double Linked List/dll.js
+++ Double Linked List/dll.js
@@ -34,4 +34,18 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        var poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
 }
