4e24191023c36e63cd2eeb142328ad101eb61b72
Ivan Okello <ivandamieno99@gmail.com>
2022-10-19 11:38:57 +0300

    Implemented the get method

--- Double Linked List/dll.js
+++ Double Linked List/dll.js
@@ -74,4 +74,27 @@ class DoublyLinkedList {
             this.head = newNode;
         }
     }
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        var count, current;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+            return current;
+
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+
+            }
+            return current;
+        }
+    }
 }
