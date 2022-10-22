f93dfca1563a75e84216df95a8be3cfa61280244
Ivan Okello <ivandamieno99@gmail.com>
2022-10-22 15:36:12 +0300

    Implemented find method but used contains, this is a little bit optimal

--- Binary Search Trees/bst.js
+++ Binary Search Trees/bst.js
@@ -52,6 +52,21 @@ class BinarySearchTree {
         if (!found) return undefined;
         return current;
     }
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true
+            }
+        }
+        return false;
+    }
 }
 
 // var tree = new BinarySearchTree();
