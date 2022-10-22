f8b9835bb6bdff10f56e67a2aaa65758f5ca432c
Ivan Okello <ivandamieno99@gmail.com>
2022-10-22 14:35:22 +0300

    Created insert method

--- Binary Search Trees/bst.js
+++ Binary Search Trees/bst.js
@@ -9,10 +9,37 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while (true) {
+                if (value === current.value) return undefined;
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
 
-var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+// var tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
