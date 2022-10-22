e88b109fbece08e13080c809b9458adc7c794a6d
Ivan Okello <ivandamieno99@gmail.com>
2022-10-22 15:28:40 +0300

    Created a find method

--- Binary Search Trees/bst.js
+++ Binary Search Trees/bst.js
@@ -36,6 +36,22 @@ class BinarySearchTree {
             }
         }
     }
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root;
+        found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
 }
 
 // var tree = new BinarySearchTree();
