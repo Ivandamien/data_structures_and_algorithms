1ce2f1f54d1d0e01f707910cd2df775e91d8975d
Ivan Okello <ivandamieno99@gmail.com>
2022-10-23 20:35:55 +0300

    Implemented DFSPostOrder

--- Tree Traversal/breadthFirstSearch.js
+++ Tree Traversal/breadthFirstSearch.js
@@ -91,6 +91,18 @@ class BinarySearchTree {
         traverse(current)
         return data;
     }
+    DFSPostOrder() {
+        var data = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
 }
 
 
