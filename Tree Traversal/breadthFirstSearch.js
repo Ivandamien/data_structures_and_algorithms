6bfd579fee22caecab89718155e91751bffe9ba2
Ivan Okello <ivandamieno99@gmail.com>
2022-10-23 20:55:46 +0300

    DFS InOrder Traversal implemented

--- Tree Traversal/breadthFirstSearch.js
+++ Tree Traversal/breadthFirstSearch.js
@@ -102,6 +102,17 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+    DFSInOrder() {
+        var data = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root);
+        return data;
+    }
 
 }
 
