772460c6dd1223bb11d1411b0a35b9dc43754f16
Ivan Okello <ivandamieno99@gmail.com>
2022-10-24 19:59:50 +0300

    Started binary heap implementation

---
+++ Binary Heap/maxBinaryHeap.js
@@ -0,0 +1,22 @@
+class maxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+    insert(element) {
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let index = this.values.length - 1;
+        const element = this.values[index];
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.values[parentIndex];
+            if (element <= parent) break;
+            this.values[parentIndex] = element;
+            this.values[index] = parent;
+            index = parentIndex;
+
+        }
+    }
+}
