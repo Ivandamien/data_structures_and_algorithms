bbb84262a4a8781cd06d2481185eed7c60a4def8
Ivan Okello <ivandamieno99@gmail.com>
2022-10-24 22:08:56 +0300

    Implemented extractMax iteratively

--- Binary Heap/maxBinaryHeap.js
+++ Binary Heap/maxBinaryHeap.js
@@ -18,5 +18,46 @@ class maxBinaryHeap {
             index = parentIndex;
 
         }
+
+    }
+    extractMax() {
+        // EDGE CASE COME BACK TO THIS
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end;
+        // trickle down
+        this.sinkDown();
+        return max;
+    }
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+
+                if ((swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
     }
 }
