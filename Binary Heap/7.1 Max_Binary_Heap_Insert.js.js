8abca13974093b65d7feb0759af635a5a759d812
Ivan Okello <ivandamieno99@gmail.com>
2022-10-29 22:54:15 +0300

    Implemented sinkdown in binary heaps

--- Binary Heap/7.1 Max_Binary_Heap_Insert.js.js
+++ Binary Heap/7.1 Max_Binary_Heap_Insert.js.js
@@ -20,17 +20,47 @@ class MaxBinaryHeap {
     }
 
     extractMax() {
-        // Edgecase come back to this
+
         const max = this.values[0];
         const end = this.values.pop();
-        this.values[0] = end;
-        this.sinkDown();
+        if (this.values.length > 0) {
+
+            this.values[0] = end;
+            this.sinkDown();
+        }
         return max;
     }
     sinkDown() {
-        let index = 0;
+        let idx = 0;
         const length = this.values.length;
         const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+                    swap = rightChildIdx;
+                }
+            }
+
+
+
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap
+        }
     }
 }
 
@@ -40,5 +70,9 @@ heap.insert(39);
 heap.insert(33);
 heap.insert(18);
 heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+heap.insert(12);
+heap.insert(55);
 heap.insert(12);
 heap.insert(55);
