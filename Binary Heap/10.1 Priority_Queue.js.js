94480f33ae5bc44da538465433e26deb06c4a0c8
Ivan Okello <ivandamieno99@gmail.com>
2022-10-29 20:24:39 +0300

    Implemented extract max

--- Binary Heap/10.1 Priority_Queue.js.js
+++ Binary Heap/10.1 Priority_Queue.js.js
@@ -1,59 +1,59 @@
 class PriorityQueue {
-    constructor(){
+    constructor() {
         this.values = [];
     }
-    enqueue(val, priority){
+    enqueue(val, priority) {
         let newNode = new Node(val, priority);
         this.values.push(newNode);
         this.bubbleUp();
     }
-    bubbleUp(){
+    bubbleUp() {
         let idx = this.values.length - 1;
         const element = this.values[idx];
-        while(idx > 0){
-            let parentIdx = Math.floor((idx - 1)/2);
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
             let parent = this.values[parentIdx];
-            if(element.priority >= parent.priority) break;
+            if (element.priority >= parent.priority) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent;
             idx = parentIdx;
         }
     }
-    dequeue(){
+    dequeue() {
         const min = this.values[0];
         const end = this.values.pop();
-        if(this.values.length > 0){
+        if (this.values.length > 0) {
             this.values[0] = end;
             this.sinkDown();
         }
         return min;
     }
-    sinkDown(){
+    sinkDown() {
         let idx = 0;
         const length = this.values.length;
         const element = this.values[0];
-        while(true){
+        while (true) {
             let leftChildIdx = 2 * idx + 1;
             let rightChildIdx = 2 * idx + 2;
-            let leftChild,rightChild;
+            let leftChild, rightChild;
             let swap = null;
 
-            if(leftChildIdx < length){
+            if (leftChildIdx < length) {
                 leftChild = this.values[leftChildIdx];
-                if(leftChild.priority < element.priority) {
+                if (leftChild.priority < element.priority) {
                     swap = leftChildIdx;
                 }
             }
-            if(rightChildIdx < length){
+            if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
-                if(
-                    (swap === null && rightChild.priority < element.priority) || 
+                if (
+                    (swap === null && rightChild.priority < element.priority) ||
                     (swap !== null && rightChild.priority < leftChild.priority)
                 ) {
-                   swap = rightChildIdx;
+                    swap = rightChildIdx;
                 }
             }
-            if(swap === null) break;
+            if (swap === null) break;
             this.values[idx] = this.values[swap];
             this.values[swap] = element;
             idx = swap;
@@ -62,21 +62,15 @@ class PriorityQueue {
 }
 
 class Node {
-    constructor(val, priority){
+    constructor(val, priority) {
         this.val = val;
         this.priority = priority;
     }
 }
 
 let ER = new PriorityQueue();
-ER.enqueue("common cold",5)
+ER.enqueue("common cold", 5)
 ER.enqueue("gunshot wound", 1)
-ER.enqueue("high fever",4)
-ER.enqueue("broken arm",2)
-ER.enqueue("glass in foot",3)
-
-
-
-
-
-
+ER.enqueue("high fever", 4)
+ER.enqueue("broken arm", 2)
+ER.enqueue("glass in foot", 3)
--- Binary Heap/7.1 Max_Binary_Heap_Insert.js.js
+++ Binary Heap/7.1 Max_Binary_Heap_Insert.js.js
@@ -1,22 +1,36 @@
 class MaxBinaryHeap {
-    constructor(){
+    constructor() {
         this.values = [];
     }
-    insert(element){
+    insert(element) {
         this.values.push(element);
         this.bubbleUp();
     }
-    bubbleUp(){
+    bubbleUp() {
         let idx = this.values.length - 1;
         const element = this.values[idx];
-        while(idx > 0){
-            let parentIdx = Math.floor((idx - 1)/2);
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
             let parent = this.values[parentIdx];
-            if(element <= parent) break;
+            if (element <= parent) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent;
             idx = parentIdx;
         }
+    }
+
+    extractMax() {
+        // Edgecase come back to this
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end;
+        this.sinkDown();
+        return max;
+    }
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
     }
 }
 
@@ -28,6 +42,3 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
-
-
-
