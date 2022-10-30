cbb0c3c21fc73ad08d283377eea1bad94ab7e2bd
Ivan Okello <ivandamieno99@gmail.com>
2022-10-30 17:27:34 +0300

    Implemented a hash function that works only on strings

--- Hash Tables/firstHash.js
+++ Hash Tables/firstHash.js
@@ -1,1 +1,12 @@
 hash("pink", 100);
+
+//Hash that works on strings only
+function hash(key, arrayLen) {
+    let total = 0;
+    for (let char of key) {
+        // map "a" to 1, "b" to 2, "c" to 3,etc.
+        let value = char.charcodeAt(0) - 96
+        total = (total + value) % arrayLen;
+    }
+    return total;
+}
