326b7efb54b4ea19f0778c6b219c95218773ab98
Ivan Okello <ivandamieno99@gmail.com>
2022-10-31 21:59:56 +0300

    Implemented the remove vertex method

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -20,6 +20,13 @@ class Graph {
             v => v !== vertex1
         );
     }
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
 
 let g = new Graph();
