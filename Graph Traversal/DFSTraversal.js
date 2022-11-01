0a4092e55c5dea84734ea2ae4b553cf1752365dd
Ivan Okello <ivandamieno99@gmail.com>
2022-11-01 15:33:55 +0300

    Implemented breadFirstSearch on graphs

--- Graph Traversal/DFSTraversal.js
+++ Graph Traversal/DFSTraversal.js
@@ -64,6 +64,25 @@ class Graph {
             return result;
         }
     }
+    breadthFirst(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        while (queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 
 }
 
