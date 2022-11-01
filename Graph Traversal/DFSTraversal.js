16fd8e94d9a044e53a690f0d867aa2ed3a2797d3
Ivan Okello <ivandamieno99@gmail.com>
2022-11-01 14:51:09 +0300

    Implemented DepthFirstIterative

--- Graph Traversal/DFSTraversal.js
+++ Graph Traversal/DFSTraversal.js
@@ -41,8 +41,28 @@ class Graph {
                 }
             })
         })(start);
-        
+
         return result;
+    }
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while (stack.length) {
+            currentVertex = stack.pop();  
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor)
+                }
+            })
+            return result;
+        }
     }
 
 }
