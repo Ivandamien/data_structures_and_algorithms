f6d38c94bcfead3df09bd98022d4ac439e6c743b
Ivan Okello <ivandamieno99@gmail.com>
2022-11-03 11:16:29 +0300

    Memoized fibonacci

---
+++ Dynamic programming/memoized_fb.js
@@ -0,0 +1,7 @@
+function fib(n, memo = []) {
+    if (memo[n] !== undefined) return memo[n];
+    if (n <= 2) return 1;
+    var res = fib(n - 1, memo) + fib(n - 2, memo);
+    memo[n] = res;
+    return res;
+}
